21caf04c42d9d1e73f5e2cc60732d9660c1e3393
erickbauer <contatoerickbauer@gmail.com>
2024-12-17 15:41:32 -0300

    implementada leitura de arquivo json

---
+++ DadosListas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompararListaDeNumeros
+{
+  internal class DadosListas
+  {
+    public List<int> lista1;
+    public List<int> lista2;
+  }
+}
